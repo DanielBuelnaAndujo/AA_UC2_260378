ce268f5ab8f77445d15bbb1569dcd0ee3bab8a6a
Daniel Buelna <>
2025-03-06 21:51:35 -0700

    Metodos de ordenamiento burbuja (documentado) e insercion.

---
+++ AA2_UC2_260378/src/aa2_uc2_260378/Busquedas.java
@@ -0,0 +1,9 @@
+package aa2_uc2_260378;
+
+/**
+ *
+ * @author daniel
+ */
+public class Busquedas {
+    
+}
---
+++ AA2_UC2_260378/src/aa2_uc2_260378/Pruebas.java
@@ -0,0 +1,20 @@
+package aa2_uc2_260378;
+
+/**
+ *
+ * @author daniel
+ */
+public class Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] a = {10, 20, 11, 12, 13};
+        //Ordenamiento.insercion(a);
+        int[] b = {5, 1, 3};
+        Ordenamiento.burbuja(b);
+        Ordenamiento.mostrar(b);
+    }
+    
+}
