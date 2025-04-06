d595ed3ac4ac11e219df7b6c1df55dc2770c803b
Daniel Buelna <guzman@ACER-GUZMAN>
2025-04-05 19:55:18 -0700

    Se añadio seleccion, quickSort y particion.

--- AA2_UC2_260378/src/algoritmos/Ordenamiento.java
+++ AA2_UC2_260378/src/algoritmos/Ordenamiento.java
@@ -25,17 +25,18 @@ public class Ordenamiento {
      */
     public static void burbuja(int[] a) {
         for (int i = 1; i < a.length; i++) { //1+(n-1+1)+(n-1) = 2n
-            for (int j = 0; j < a.length-i; j++) { //n*(1+n+1+n) = 2n^2+2n
+            for (int j = 0; j < a.length-i; j++) { //(n-1)*((1)+(n-i+1)+(n-i)) = (n-1)*(2n-2i+2) = (2n^2 - 2ni + 2n) - (2n-2i+2) = 2n^2-2ni+2i-2
                 // condicion
-                if (a[j]>a[j+1]) { //n*n = n^2
+                if (a[j]>a[j+1]) { //(n-i)*1 = n-i
                     // Intercambio
-                    int aux = a[j]; //n^2
-                    a[j] = a[j+1]; //n^2
-                    a[j+1] = aux; //n^2
+                    int aux = a[j]; //(n-i)*1 = n-i
+                    a[j] = a[j+1]; //(n-i)*2 = 2n-2i
+                    a[j+1] = aux; //(n-i)*2 = 2n-2i
                 }
             }
         }
-    } //(2n)+(2n^2+2n)+(n^2)+(n^2)+(n^2) = 5n^2+4n O(n^2)
+    } //(2n)+(2n^2-2ni+2i-2)+2(n-i)+2(2n-2i) = (2n)+(2n^2-2ni+2i-2)+(2n-2i)+(4n-4i) = 2n^2-2ni+8n-4i-2
+      //O(n^2)
     
     /**
      * Método de ordenamiento por insercion
@@ -51,31 +52,117 @@ public class Ordenamiento {
     public static void insercion(int [] a) {
         for (int i = 1; i < a.length; i++) { //1+(n-1+1)+(n-1) = 2n
             int key = a[i]; //2n
-            int j = i-1; //2n
+            int j = i-1; //2n+1
             while (j>=0 && a[j]>key) { //2n*(n) = 2n^2
                 a[j+1] = a[j]; //2n^2
                 j--; //2n^2
             }
-            a[j+1] = key; //2n
-        } // (2n)+(2n)+(2n)+(2n^2)+(2n^2)+(2n^2)+(2n)
-          // = 4(2n)+3(2n^2)
-          // = (8n)+(6n^2) O(n^2)  
-    }
+            a[j+1] = key; //2n+1
+        } 
+    } // (2n)+(2n)+(2n)+(2n^2)+(2n^2)+(2n^2)+(2n)
+      // = 4(2n)+3(2n^2)
+      // = (8n)+(6n^2) O(n^2)  
     
     // a.length = 6
+    /**
+     * Método de ordenamiento por selección
+     * 
+     * Precondición: El arreglo no puede estar vacío.
+     * Postcondición: El arreglo debe de estar ordenado.
+     * 
+     * @param a El arreglo de enteros a entrar.
+     * @param n El tamaño del arreglo
+     * @param minIndex El indice del número menor.
+     * @param aux Funciona como auxiliar para el valor de a[miniIndex].
+     * @param i Recorre cada valor del arreglo y se compara con j.
+     * @param j Recorre cada valor del arreglo y se compara con i.
+     */
     public static void seleccion(int[] a) {
         int n = a.length; //1
-        for (int i = 0; i < n-1; i++) { //Nos va a dar la posición del elemento más pequeño del arreglo
-            int minIndex = i;
-            for (int j = i+1; j < n; j++) {
-                if (a[j]<a[minIndex]) {
-                    minIndex = j;
+        for (int i = 0; i < n-1; i++) { //2n   Nos va a dar la posición del elemento más pequeño del arreglo
+            int minIndex = i; //2n(1) = 2n 
+            for (int j = i+1; j < n; j++) { //n((1)+(n-i+1+1)+(n-i+1)) = n(2n-2i+4) = 2n^2-2i+4
+                if (a[j]<a[minIndex]) { //(n-i+1)*(1) = n-i+1
+                    minIndex = j; //(n-i+1)*(1) = n-i+1
                 }
             }
             //Intercambio
-            int aux = a[minIndex];
-            a[minIndex] = a[i];
-            a[i] = aux;
+            int aux = a[minIndex]; //(n-1)*(1) = n-1
+            a[minIndex] = a[i]; //(n-1)*(1) = n-1
+            a[i] = aux; //(n-1)*(1) = n-1
         }
+    } //(1)+2(2n)+(2n^2-2i+4)+2(n-i+1)+3(n-1) = (4n)+(2n^2-2i+4)+(2n-2i+2)+(3n-3) = 2n^2+9n-4i+3
+      //O(n^2)
+    
+    /**
+     * Método de ordenamiento quickSort
+     * 
+     * Precondición: Las variables a, desde y hasta no pueden estar vacíos.
+     * Postcondición: El arreglo debe de estar ordenado.
+     * 
+     * @param a El arreglo de enteros a entrar.
+     * @param desde El inicio del arreglo.
+     * @param hasta El final del arreglo.
+     * @param pivote La mitad del arreglo en su mayoria.
+     */
+    public static void quickSort(int[] a, int desde, int hasta) {
+        //Caso base
+        if (desde<hasta) { //1
+        //Caso Recursivo
+            //Obtener pivote
+            ////lo necesito en cada particion
+            int pivote = particionar(a, desde, hasta); //1 op
+            //aplicar algoritmo en el segmento de la izquierda
+            quickSort(a, desde, pivote-1); //1
+
+            //aplicar algoritmo en el segmento de la derecha
+            quickSort(a, pivote+1, hasta); //1
+        }
+    } 
+    // O(nLogn)
+    
+    /**
+     * Metodo para particionar y devolver el indice del pivote
+     * 
+     * Precondición: Las variables a, desde y hasta no pueden estar vacíos.
+     * Postcondición: Un número entero positivo.
+     * 
+     * @param a El arreglo de enteros a entrar.
+     * @param desde El inicio del arreglo.
+     * @param hasta El final del arreglo.
+     * @param pivote El valor con el que se comparan los demas valores del arreglo.
+     * @param i Sirve para intercambiar valores con j y aumenta de 1 en 1.
+     * @param j Recorre el arreglo y se compara con i.
+     * @param aux Guarda el valor de a[i].
+     * @param temp Guarda el valor de a[i+1].
+     */
+    public static int particionar(int[] a, int desde, int hasta) {
+        //seleccionar el pivote
+        int pivote = a[hasta]; //1
+        //seleccionar el indice mas pequeño
+        int i = desde-1; //** //2
+        
+        //recorrer todo el arreglo buscando elementos menores al pivote
+        //para intercambiarlos
+        for (int j = desde; j < hasta; j++) {
+            //a[j] marca el valor actual con el que se compara el pivote
+            if (a[j] < pivote) {
+                i++;
+                int aux = a[i]; //1
+                a[i]=a[j]; //1
+                a[j]=aux; //1
+            }
+        }
+        //ultimo intercambio para colocar el pivote
+        int temp = a[i+1]; //2
+        a[i+1] = a[hasta]; //2
+        a[hasta] = temp; //1
+        
+        //regresar el inicio del pivote
+        return i+1; //1
     }
+    
+    //nLog n
+    //n es el numero de veces que se selecciona el pivote y
+    //Log n son las particiones de mitad en mitad que hacemos
 }
--- AA2_UC2_260378/src/algoritmos/Pruebas.java
+++ AA2_UC2_260378/src/algoritmos/Pruebas.java
@@ -10,12 +10,53 @@ public class Pruebas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] a = {10, 20, 11, 12, 13};
+        int[] b = {10, 20, 11, 12, 13};
         //Ordenamiento.insercion(a);
-        int[] b = {5, 1, 3};
+        int[] a = {5, 1, 3};
         int[] c = {12, 20, 11, 13, 10};
-        Ordenamiento.insercion(c);
-        Ordenamiento.mostrar(c);
+        int[] d = {7, 8, 3, 1, 2, 9, 4};
+        int[] e = {1, 2, 3, 4, 5, 6, 7};
+        int n = a.length;
+//        Ordenamiento.quickSort(d, 0, n-1);
+//        quickSort(a, 0, n-1);
+//        Ordenamiento.mostrar(a);
+//        ciclos(new int[4]);
+        prueba(b);
+    }
+    
+    public static void prueba(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            System.out.println("AA: " + i);
+            for (int j = 0; j < a.length-i; j++) {
+                System.out.println(j);   
+            }
+        }
+    }
+    
+    public static void ciclos(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n-1; i++) {
+            System.out.println("AA: " + i);
+            for (int j = i+1; j < n; j++) {
+                System.out.println(j);
+            }
+        }
+    }
+    
+    public static void seleccion(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (a[j]<a[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            //Intercambio
+            int aux = a[minIndex];
+            a[minIndex] = a[i];
+            a[i] = aux;
+        }
     }
     
 }
